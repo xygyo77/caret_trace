34fbf2d059dac15af228d2e368249837fd11e1dd
ISP akm <akm@isp.co.jp>
2026-04-11 16:19:52 +0900

    add debug
    Signed-off-by: ISP akm <akm@isp.co.jp>

--- CARET_trace/src/hooked_trace_points.cpp
+++ CARET_trace/src/hooked_trace_points.cpp
@@ -240,10 +240,17 @@ void update_dds_function_addr()
       "_ZN8eprosima8fastrtps4rtps13WriterHistory13set_fragmentsEPNS1_13CacheChange_tE");  // NOLINT
     // clang-format on
   } else if (env_var == "rmw_cyclonedds_cpp") {
+// ログ1: ここを通っているか
+    std::cerr << "[CARET_DEBUG] RMW is cyclonedds. Loading libddsc.so..." << std::endl;
+
     static rcpputils::SharedLibrary ddsc_lib("libddsc.so");
     
+    // ログ2: シンボルが取れているか
     CYCLONEDDS::DDS_WRITE_TS = ddsc_lib.get_symbol("dds_write_ts");
+    std::cerr << "[CARET_DEBUG] dds_write_ts addr: " << CYCLONEDDS::DDS_WRITE_TS << std::endl;
+
     CYCLONEDDS::DDS_WRITECDR_IMPL = lib->get_symbol("dds_writecdr_impl");
+    std::cerr << "[CARET_DEBUG] dds_writecdr_impl addr: " << CYCLONEDDS::DDS_WRITECDR_IMPL << std::endl;
   }
 }
 
@@ -253,6 +260,9 @@ void update_dds_function_addr()
 // bind : &ros_message -> source_timestamp
 int dds_write_ts(void * wr, void * data, long tstamp)  // NOLINT
 {
+  std::cerr << "[CARET_DEBUG] dds_write_ts CALLED! data_addr: " << data 
+            << ", ts: " << timestamp << std::endl;
+
   static auto & context = Singleton<Context>::get_instance();
   using functionT = int (*)(void *, void *, long);  // NOLINT
 
